fbfdc47ec69720001019b9b94a981a2809e6dd39
Cieran0 <cieran999@gmail.com>
2024-08-20 17:31:16 +0100

    Add a couple functions.

--- raylib_wrapper.c
+++ raylib_wrapper.c
@@ -38,3 +38,26 @@ void raylib_DrawCircle(int centerX, int centerY, int radius, rgba) {
 void raylib_SetTargetFPS(int fps) {
     SetTargetFPS(fps);
 }
+
+void raylib_DrawRectangle(int x, int y, int width, int height, rgba) {
+    DrawRectangle(x,y,width,height,color_struct);
+}
+
+bool raylib_CheckCollisionRecs(int x_1, int y_1, int width_1, int height_1, int x_2, int y_2, int width_2, int height_2) {
+    return CheckCollisionRecs((Rectangle){x_1, y_1, width_1, height_1}, (Rectangle){x_2, y_2, width_2, height_2} );
+}
+
+int raylib_GetRandomValue(int min, int max) {
+    return GetRandomValue(min,max);
+}
+
+void int_to_string(int i, char* string_buffer) {
+    const char* as_string = TextFormat("%i", i);
+    int index = 0;
+    while (as_string[index] != 0)
+    {
+        string_buffer[index] = as_string[index];
+        index++;
+    }
+    string_buffer[index] = 0;
+}
